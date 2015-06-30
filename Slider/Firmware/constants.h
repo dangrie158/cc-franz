90ccfbb3b5c28b488e706cab43713a31c2acaf6a
Daniel Grießhaber <dangrie158@gmail.com>
2015-06-30 09:04:39 +0200

    added uart functions and simple parsing of the mesage format

--- Slider/Firmware/constants.h
+++ Slider/Firmware/constants.h
@@ -1,8 +1,2 @@
 #define DIR_LEFT 1
 #define DIR_RIGHT 0
-
-#define BAUD 9600L      // Baudrate
-#define UART_MAXSTRLEN 47
-// Berechnungen
-#define UBRR_VAL ((F_CPU+BAUD*8)/(BAUD*16)-1)   // F_CPU is defined in make
-#define BAUD_REAL (F_CPU/(16*(UBRR_VAL+1)))     // real baudrate
