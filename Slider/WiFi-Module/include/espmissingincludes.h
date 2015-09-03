74ea828472cbdb49175a9ab99c4a4a115dcd7806
Daniel Grießhaber <dangrie158gmail.com>
2015-09-03 22:15:41 +0200

    added latest version of WiFi Firmware

--- Slider/WiFi-Module/include/espmissingincludes.h
+++ Slider/WiFi-Module/include/espmissingincludes.h
@@ -41,8 +41,6 @@ void vPortFree(void *ptr);
 void *vPortMalloc(size_t xWantedSize);
 uint8 wifi_get_opmode(void);
 uint32 system_get_time();
-int os_random();
-int rand(void);
 void ets_bzero(void *s, size_t n);
 void ets_delay_us(int ms);
 bool system_os_task(os_task_t task, uint8 prio, os_event_t *queue, uint8 qlen);
